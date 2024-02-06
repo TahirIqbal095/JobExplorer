04b2ca65f57ba86ca15959430c544fe7b6753f6e
Tahir iqbal <shahtahir786@gmail.com>
2024-02-06 14:20:43 +0530

    get and post mapping

--- src/main/java/com/tahir/explorejobs/job/Job.java
+++ src/main/java/com/tahir/explorejobs/job/Job.java
@@ -1,2 +1,67 @@
-package com.tahir.explorejobs.job;public class Job {
+package com.tahir.explorejobs.job;
+
+public class Job {
+    private int id;
+    private String title;
+    private String discription;
+    private String minSalary;
+    private String maxSalary;
+    private String location;
+
+    public Job(int id, String title, String discription, String minSalary, String maxSalary, String location) {
+        this.id = id;
+        this.title = title;
+        this.discription = discription;
+        this.minSalary = minSalary;
+        this.maxSalary = maxSalary;
+        this.location = location;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDiscription() {
+        return discription;
+    }
+
+    public void setDiscription(String discription) {
+        this.discription = discription;
+    }
+
+    public String getMinSalary() {
+        return minSalary;
+    }
+
+    public void setMinSalary(String minSalary) {
+        this.minSalary = minSalary;
+    }
+
+    public String getMaxSalary() {
+        return maxSalary;
+    }
+
+    public void setMaxSalary(String maxSalary) {
+        this.maxSalary = maxSalary;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
--- src/main/java/com/tahir/explorejobs/job/JobController.java
+++ src/main/java/com/tahir/explorejobs/job/JobController.java
@@ -1,5 +1,42 @@
 package com.tahir.explorejobs.job;
 
-public class JobController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class JobController {
+    private List<Job> jobs = new ArrayList<>();
+
+    @GetMapping("/jobs")
+    public List<Job> findAll() {
+        return jobs;
+    }
+    @PostMapping("/jobs")
+    public String createJob(@RequestBody Job job) {
+        jobs.add(job);
+        return "job added successfully";
+    }
 }
+
+
+
+
+/*
+
+get /jobs: get all jobs
+get /jobs/{id}: get a specific job by id
+
+post /jobs: create a new job (request body should contain the job details)
+
+delete /jobs/{id}: delete a specific job by id
+
+put /jobs/{id}: update a specific job with id (request body should contain the updated job details)
+
+get /jobs/{id}/company : get the company associated with specific job by id
+
+ */
